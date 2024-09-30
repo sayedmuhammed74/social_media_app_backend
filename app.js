7369b174f8524528fab321e5b54a35a1cedc9153
unknown <sayedmohamed123.sm74@gmail.com>
2024-09-30 12:17:46 +0300

    likes & comments

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
     credentials: true, // This allows cookies to be sent and received
   })
 );
-
+app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 // routes
